59e734dea1bdd00b8cc6996bcb005cfc444b391a
Utsav Varshney <utsav.varshney@nagarro.com>
2022-12-28 21:37:26 +0530

    first commit

---
+++ src/childclass.java
@@ -0,0 +1,7 @@
+
+public class childclass extends parentclass{
+    public static void main(String[] args) {
+        display();
+        System.out.println("In child class");
+    }
+}
---
+++ src/parentclass.java
@@ -0,0 +1,8 @@
+
+public class parentclass {
+    static int c=25;
+    public static void display() {
+        System.out.println("In parent class");
+        System.out.println("Value of c= "+c);
+    }
+}
